5ace475d38d43eb0edaa550dff50062ceb9df12e
РудневИ <irudnev@protonmail.com>
2021-07-01 21:49:58 +0600

    check/pay

--- LockStep/Controllers/WebApi/BooksController.cs
+++ LockStep/Controllers/WebApi/BooksController.cs
@@ -4,6 +4,7 @@ using LockStep.Library.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,8 +12,24 @@ using System.Web.Mvc;
 
 namespace LockStep.Web.Controllers.WebApi
 {
+
+    [Serializable]
+    public class QueryResult
+    {
+        
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public QueryResult(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
     public class BooksController : ApiController
     {
+
         // GET: Books
         private readonly LibraryDbContext _context = new LibraryDbContext();
         
@@ -22,19 +39,32 @@ namespace LockStep.Web.Controllers.WebApi
             {
                 List<Book> books = _context.Books.ToList();
                 List<Price> prices = _context.Prices.ToList();
-                
-                var query = from book in books
-                            join price in prices
-                            on book.Id equals price.Book.Id
-                            where price.To == null
-                            select new
-                            {
-                                book?.Name,
-                                price?.Value
-                            };
-                
 
-                return Ok(query?.ToList());
+
+                List<QueryResult> result = _context.Books.Join(
+                   _context.Prices,
+                   book => book.Id,
+                   price => price.Book.Id,
+                   (book, price) => new
+                   QueryResult(book.Name, price.Value)
+                   )
+                    .ToList()
+                    
+                    
+                    ;
+/*
+                IEnumerable<QueryResult>query = ((IEnumerable<QueryResult>)(from book in books
+                             join price in prices
+                             on book.Id equals price.Book.Id
+                             where price.To == null
+                             select new
+                             {
+                                 book?.Name,
+                                 price?.Value
+                             }));
+*/
+               /* Book book = books.Where(b => b.Id == id).FirstOrDefault();*/
+                return Ok(result);
             }
             catch (Exception e)
             {
@@ -44,5 +74,6 @@ namespace LockStep.Web.Controllers.WebApi
 
 
     }
+   
 
 }
---
+++ LockStep/Controllers/WebApi/PayController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LockStep.Web.Controllers.WebApi
+{
+    public class PayController : ApiController
+    {
+
+
+        public IHttpActionResult Get(int id, double sum, string email) 
+        {
+            try
+            {
+               
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return InternalServerError(e);
+            }
+        
+        }
+
+
+    }
+}
